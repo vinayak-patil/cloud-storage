cdb83697798fe0c9d6b352fb1e222c6529daccf0
Vinayak Patil <vinayak.patil@tekditechnologies.com>
2025-06-19 20:04:41 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './cloud-storage.module';
-export * from './interfaces/cloud-storage.interface';
+export * from './storage/interfaces/cloud-storage.interface';
 export * from './services/aws-s3.service';
 export * from './controllers/cloud-storage.controller';
